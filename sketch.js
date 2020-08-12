d302a2981777cbedfe3ceca2bff8affe7fb41a17
kushagra21-08 <67947970+kushagra21-08@users.noreply.github.com>
2020-08-12 09:24:30 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.7, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
 	World.add(world, packageBody);
 	
 
@@ -60,6 +60,10 @@ function keyPressed() {
 	Matter.Body.setStatic(packageBody, false);
   }
 }
+
+
+
+
 
 
 
